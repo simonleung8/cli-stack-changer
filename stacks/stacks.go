38a40a3db5eb7588a71f3816280eb9ad02472728
Simon Leung <leungs@us.ibm.com>
2015-04-01 18:39:52 -0700

    stacks package
    [#91450530]
    
    Signed-off-by: Jonathan Berkhahn <jaberkha@us.ibm.com>

---
+++ stacks/stacks.go
@@ -0,0 +1,78 @@
+package stacks
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/cloudfoundry/cli/plugin"
+)
+
+const (
+	lucid64    = "lucid64"
+	cflinuxfs2 = "cflinuxfs2"
+)
+
+type StacksModel struct {
+	Resources []StackModel `json:"resources"`
+}
+
+type StackModel struct {
+	Metadata MetadataModel `json:"metadata"`
+	Entity   EntityModel   `json:"entity"`
+}
+
+type MetadataModel struct {
+	Guid string `json:"guid"`
+}
+
+type EntityModel struct {
+	Name string `json:"name"`
+}
+
+type Stacks interface {
+	GetLucid64Guid() (string, error)
+	GetCflinuxfs2Guid() (string, error)
+}
+
+type stacks struct {
+	cliCon plugin.CliConnection
+}
+
+func NewStacks(cliConnection plugin.CliConnection) Stacks {
+	return &stacks{
+		cliCon: cliConnection,
+	}
+}
+
+func (s *stacks) GetLucid64Guid() (string, error) {
+	return s.getStackGuid(lucid64)
+}
+
+func (s *stacks) GetCflinuxfs2Guid() (string, error) {
+	return s.getStackGuid(cflinuxfs2)
+}
+
+func (s *stacks) getStackGuid(name string) (string, error) {
+	output, err := s.cliCon.CliCommandWithoutTerminalOutput("curl", "/v2/stacks")
+	if err != nil {
+		return "", err
+	}
+
+	model := StacksModel{}
+	err = json.Unmarshal([]byte(output[0]), &model)
+	if err != nil {
+		return "", err
+	}
+
+	if len(model.Resources) == 0 {
+		return "", errors.New("No stacks named " + name + " is found")
+	}
+
+	for _, s := range model.Resources {
+		if s.Entity.Name == name {
+			return s.Metadata.Guid, nil
+		}
+	}
+
+	return "", nil
+}
